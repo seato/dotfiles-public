362775f1d812d2dce7c2b0fca4e5b0697797cf60
Richard Thai <richard.thai@discordapp.com>
2022-07-19 15:56:43 -0700

    bash if statements lol

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y emacs
 
 # Pure theme for ZSH
 mkdir -p "$HOME/.zsh"
-if [ ! -d "$HOME/.zsh/pure" ] 
+if [ ! -d "$HOME/.zsh/pure" ]
+then
 	git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 fi
